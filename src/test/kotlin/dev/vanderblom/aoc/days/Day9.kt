7f5dc46de2d82ef6aaf0f96b917c91810ef3135e
Martijn van der Blom <martijn@teamofteams.nl>
2024-12-09 15:28:05 +0100

    Day9 pt 2 - I'm not proud of myself

--- src/test/kotlin/dev/vanderblom/aoc/days/Day9.kt
+++ src/test/kotlin/dev/vanderblom/aoc/days/Day9.kt
@@ -18,59 +18,64 @@ class Day9 : AbstractDay() {
     @Order(2)
     fun `part one - actual`() {
         assertThat(partOne(actualInput[0]))
-            .isEqualTo(6430446922192) //90950452031; too low
+            .isEqualTo(6430446922192) //90950452031 too low;
     }
 
     @Test
     @Order(3)
     fun `part two - example`() {
         assertThat(partTwo(exampleInput[0]))
-            .isEqualTo(1)
+            .isEqualTo(2858)
     }
 
     @Test
     @Order(4)
     fun `part two - actual`() {
         assertThat(partTwo(actualInput[0]))
-            .isEqualTo(1)
+            .isEqualTo(6460170593016L) //6460120102583 too low; 7506186446615 too high;
     }
 
     private fun partOne(input: String): Long {
-        val diskMap = DiskMap(input)
-        return diskMap.compact().checksum()
+        val diskMap = DiskMap.of(input)
+        return diskMap.compactBlockLevel().checksum()
     }
 
     private fun partTwo(input: String): Long {
-        return 1
+        val diskMap = DiskMap.of(input)
+        return diskMap.compactFileLevel().checksum()
     }
 }
 
-fun List<String>.checksum() = this.filter { it != "." }
-    .map { it.toString().toLong() }
+fun List<String>.checksum() = this
+    .map {
+        if(it == ".") 0L
+        else it.toLong()
+    }
     .mapIndexed { index, fileId -> index * fileId}
     .sum()
 
-class DiskMap(val input: String) {
-    val blocks: MutableList<Block>
 
-    init {
-        var fileCounter = 0L
-        blocks = input
-            .mapIndexed { index, s ->
-                var id: Long? = null
-                var type = Block.Type.EMPTY_SPACE
-                if (index % 2 == 0) {
-                    id = fileCounter++
-                    type = Block.Type.FILE
-                }
-                Block(id, s.toString().toLong(), type)
-            }
-            .toMutableList()
+fun List<Block>.print() {
+    println(toLine().joinToString(""))
+}
+
+fun List<Block>.toLine() = flatMap { block ->
+    (1..block.size).map {
+        if (block.type == Block.Type.FILE) {
+            block.id.toString()
+        } else {
+            "."
+        }
     }
+}
 
-    fun compact(): List<String> {
-        val line = toLine()
-        val dataBlocks = toLine()
+fun List<Block>.size() = this.sumOf { it.size }
+
+
+class DiskMap(val blocks: List<Block>) {
+    fun compactBlockLevel(): List<String> {
+        val line = blocks.toLine()
+        val dataBlocks = blocks.toLine()
             .filter { it != "." }
             .toMutableList()
         val dataBlockCount = dataBlocks.size
@@ -82,21 +87,80 @@ class DiskMap(val input: String) {
         }
     }
 
-
-    fun size() = blocks.sumOf { it.size }
-
-    fun print(): DiskMap {
-        println(toLine().joinToString(""))
-        return this
+    fun <T> MutableList<T>.replaceAt(index: Int, n: Int=1, vararg newItems: T) {
+        val tempList = this.subList(0, index) + newItems + this.subList(index + n, this.size)
+        this.clear()
+        this.addAll(tempList)
     }
 
-    fun toLine() = blocks.flatMap { block ->
-        (1..block.size).map {
-            if (block.type == Block.Type.FILE) {
-                block.id.toString()
-            } else {
-                "."
-            }
+    fun compactFileLevel(): List<String> {
+        val dataBlocksReversed = blocks
+            .filter { it.type == Block.Type.FILE}
+            .reversed()
+            .toMutableList()
+
+        val processedBlocks = mutableSetOf<Block>()
+
+        val target = blocks.toMutableList()
+        dataBlocksReversed.forEach{ blockToPlace ->
+            if(processedBlocks.contains(blockToPlace)) return@forEach
+            processedBlocks.add(blockToPlace)
+
+            val oldIndex = target.indexOf(blockToPlace)
+
+            target
+                .take(oldIndex+1)
+                .mapIndexed { index, targetBlock -> index to targetBlock }
+                .firstOrNull{ (_, targetBlock) -> targetBlock.type == Block.Type.EMPTY_SPACE && targetBlock.size >= blockToPlace.size }
+                ?.also {(targetIndex, targetBlock) ->
+
+                    var newEmptyBlockSize = blockToPlace.size
+                    var newEmptyBlockStartIndex = oldIndex
+                    var replaceNItems = 1
+                    target.getOrNull(oldIndex - 1)
+                        ?.takeIf { it.type == Block.Type.EMPTY_SPACE && (oldIndex-1) != targetIndex }
+                        ?.also { blockBefore ->
+                            newEmptyBlockSize += blockBefore.size
+                            newEmptyBlockStartIndex -= 1
+                            replaceNItems++
+                        }
+                    target.getOrNull(oldIndex + 1)
+                        ?.takeIf { it.type == Block.Type.EMPTY_SPACE }
+                        ?.also { blockAfter ->
+                            newEmptyBlockSize += blockAfter.size
+                            replaceNItems++
+                        }
+                    target.replaceAt(newEmptyBlockStartIndex, replaceNItems, Block(null, newEmptyBlockSize, Block.Type.EMPTY_SPACE))
+
+                    if(targetBlock.size == blockToPlace.size) {
+                        target.replaceAt(targetIndex, 1, blockToPlace)
+                    } else {
+                        target.replaceAt(
+                            targetIndex,
+                            1,
+                            blockToPlace,
+                            targetBlock.copy(size = targetBlock.size - blockToPlace.size)
+                        )
+                    }
+                }
+        }
+        return target.toLine()
+    }
+
+    companion object {
+        fun of(input: String): DiskMap {
+            var fileCounter = 0L
+            val blocks = input
+                .mapIndexed { index, s ->
+                    var id: Long? = null
+                    var type = Block.Type.EMPTY_SPACE
+                    if (index % 2 == 0) {
+                        id = fileCounter++
+                        type = Block.Type.FILE
+                    }
+                    Block(id, s.toString().toLong(), type)
+                }
+            return DiskMap(blocks)
         }
     }
 }
