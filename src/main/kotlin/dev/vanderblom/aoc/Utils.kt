a0d378a284ad53fdeeb2f03b05b44a82e422ae5e
Martijn van der Blom <martijn@teamofteams.nl>
2024-12-10 11:18:47 +0100

    Fixes day 9

--- src/main/kotlin/dev/vanderblom/aoc/Utils.kt
+++ src/main/kotlin/dev/vanderblom/aoc/Utils.kt
@@ -61,12 +61,6 @@ fun <T> List<T>.listAfter(index: Int) = takeIf { index <= lastIndex }
     ?.let { subList(index, size) }
     ?: emptyList()
 
-fun <T> MutableList<T>.replaceAt(index: Int, n: Int=1, newItems: List<T>) {
-    val tempList = this.subList(0, index) + newItems + this.subList(index + n, this.size)
-    this.clear()
-    this.addAll(tempList)
-}
-
 infix fun <T> T.not(block: T.() -> Boolean) = block(this).not()
 
 fun <T> T?.toBoolean() = this?.let { true } ?: false
