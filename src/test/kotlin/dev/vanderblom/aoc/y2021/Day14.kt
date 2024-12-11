7788bf431eb5bf356273f7cacdc7bd1d4cbceafb
Martijn van der Blom <martijn@teamofteams.nl>
2024-12-11 22:54:47 +0100

    Cleanup 2021.14

--- src/test/kotlin/dev/vanderblom/aoc/y2021/Day14.kt
+++ src/test/kotlin/dev/vanderblom/aoc/y2021/Day14.kt
@@ -34,31 +34,13 @@ class Day14 : AbstractDay() {
         assertThat(performPairInsertion(actualInput, 40))
             .isEqualTo(1976896901756L)
     }
-    data class Data(val initialValue: String, val rules: Map<String, Char>) {
-        companion object{
 
-        }
-    }
     private fun performPairInsertion(input: List<String>, n: Int): Long {
-        val inputStr = input[0]
 
-        val rules = input
-            .subList(2, input.size)
-            .associate {
-                val vals = it.split(" -> ")
-                vals[0] to vals[1]
-            }
+        val data = Data.of(input)
 
-        fun String.morph() = listOf(this[0]+rules[this]!!, rules[this]!!+this[1])
-
-        val elementCount = inputStr.groupingBy { it }.eachCount().mapValues { it.value.toLong() }.toMutableMap()
-
-        val pairCount = inputStr
-            .windowed(2, step = 1)
-            .groupingBy { it }
-            .eachCount()
-            .mapValues { (_, count) -> count.toLong() }
-            .toMutableMap()
+        val elementCount = data.elementCount.toMutableMap()
+        val pairCount = data.pairCount.toMutableMap()
 
         repeat(n) {
             pairCount
@@ -67,15 +49,14 @@ class Day14 : AbstractDay() {
                     pairCount[oldPair] = pairCount[oldPair]!! - oldPairCount
                     if (pairCount[oldPair] == 0L) pairCount.remove(oldPair)
 
-                    val newElement = rules[oldPair]!![0]
+                    val newElement = data[oldPair]!!
                     elementCount.compute(newElement, { _, count -> count?.plus(oldPairCount) ?: oldPairCount })
-                    oldPair
-                        .morph()
+                    data
+                        .morph(oldPair)
                         .forEach { newValue ->
                             pairCount.compute(newValue, { _, count -> count?.plus(oldPairCount) ?: oldPairCount })
                         }
                 }
-            println(pairCount)
         }
 
         val elementsSortedByCount =
@@ -84,4 +65,36 @@ class Day14 : AbstractDay() {
         return elementsSortedByCount.last().second - elementsSortedByCount.first().second
     }
 
+}
+
+data class Data(val initialValue: String, val rules: Map<String, Char>) {
+
+    operator fun get(key: String) = rules[key]
+
+    fun morph(string: String) = listOf(string[0]+""+rules[string]!!, rules[string]!!+""+string[1])
+
+    val elementCount = initialValue.groupingBy { it }.eachCount().mapValues { it.value.toLong() }
+
+    val pairCount = initialValue
+        .windowed(2, step = 1)
+        .groupingBy { it }
+        .eachCount()
+        .mapValues { (_, count) -> count.toLong() }
+
+    companion object{
+        fun of(input: List<String>): Data {
+            val inputStr = input[0]
+
+            val rules = input
+                .subList(2, input.size)
+                .associate {
+                    val vals = it.split(" -> ")
+                    vals[0] to vals[1][0]
+                }
+            return Data(
+                inputStr,
+                rules
+            )
+        }
+    }
 }
